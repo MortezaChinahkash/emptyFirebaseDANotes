b2d04033e12054a4869c8703dbaa30f3a16917d8
MortezaChinahkash <sascha.chinahkash@gmail.com>
2025-06-14 14:04:38 +0200

    Add Firestore and RxJS imports to NoteListService

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import {Note} from '../interfaces/note.interface';
+import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
